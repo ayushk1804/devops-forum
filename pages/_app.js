79e731b9bc3d08c99a5bf0e7a921ad57442d08b1
Aayush Kumar <ayushk780@gmail.com>
2021-05-09 22:42:30 +0530

    added initial auth
    Signed-off-by: Aayush Kumar <ayushk780@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import 'tailwindcss/tailwind.css'
+import "tailwindcss/tailwind.css";
+import { AuthProvider } from "../src/Context/auth";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ src/Context/auth.js
@@ -0,0 +1,42 @@
+import { createContext, useContext, useReducer } from "react";
+
+const AuthDispatchContext = createContext();
+const AuthStateContext = createContext();
+
+const initialState = {
+  isAuthenticated: false,
+};
+
+function reducer(state, { payload, type }) {
+  switch (type) {
+    default:
+      throw new Error(`Unhandled action type ${type}`);
+  }
+}
+
+function AuthProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <AuthDispatchContext.Provider value={state}>
+      <AuthStateContext.Provider value={state}>
+        {children}
+      </AuthStateContext.Provider>
+    </AuthDispatchContext.Provider>
+  );
+}
+
+function useAuthDispatch() {
+  const context = useContext(AuthDispatchContext);
+  if (context === undefined)
+    throw new Error("useAuthDispatch must be used within an AuthProvider");
+  return context;
+}
+
+function useAuthState() {
+  const context = useContext(AuthStateContext);
+  if (context === undefined)
+    throw new Error("useAuthState must be used within an AuthProvider");
+  return context;
+}
+
+export { AuthProvider, useAuthDispatch, useAuthState };
